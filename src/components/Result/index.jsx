e6e4a57a3929d47fe3d2f6864c7687d254e3c247
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-04-21 12:45:14 -0700

    fix spacing for diff length of info

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -12,6 +12,8 @@ function Result() {
     weight_min: null,
     diet: null,
     habitat: null,
+    geo_range: null,
+    lifespan: null,
     id: 0,
   });
 
@@ -19,7 +21,7 @@ function Result() {
     fetch("https://zoo-animal-api.herokuapp.com/animals/rand")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setInfo({
           name: data.name,
           latinName: data.latin_name,
@@ -30,8 +32,17 @@ function Result() {
           weight_min: data.weight_min,
           diet: data.diet,
           habitat: data.habitat,
+          geo_range: data.geo_range,
+          lifespan: data.lifespan,
           id: data.id,
         });
+      });
+    fetch("https://zoo-animal-api.herokuapp.com/animals/rand/3")
+      .then((res) => res.json())
+      .then((data) => {
+        let allAnimals = data;
+
+        console.log(allAnimals[0]);
       });
   };
 
@@ -45,6 +56,8 @@ function Result() {
     weight_min,
     diet,
     habitat,
+    geo_range,
+    lifespan,
     id,
   } = info;
 
@@ -58,8 +71,8 @@ function Result() {
 
       <section className="outer nocturnal">
         <div className="nameContainer">
-          <h3 className="latinName">Latin Name: {latinName}</h3>
           <h3 className="name">{name}</h3>
+          <h3 className="latinName">Latin Name: {latinName}</h3>
         </div>
         <div className="animalContainer">
           <img src={url} alt="animal" className="animalImg"></img>
@@ -77,6 +90,8 @@ function Result() {
         <div className="mainInfo">
           <h4>Diet: {diet}</h4>
           <h4>Habitat: {habitat}</h4>
+          <h4>Geo-Range: {geo_range}</h4>
+          <h4>Lifespan: {lifespan} years</h4>
         </div>
 
         <span className="cardNum">{id} / 200</span>
